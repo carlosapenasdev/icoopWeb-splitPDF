e175fb50eeb9494c83e8f393ca3abc4b88734403
carlos santos <carlos.santos.wd@gmail.com>
2019-07-29 13:37:19 -0300

    removing redundancies
    Now only one function can handle with the both (or more) cases of split

--- funcoes/splitPDF.php
+++ funcoes/splitPDF.php
@@ -1,5 +1,6 @@
 <?php
-function splitPdfByEmployeeCode($pdfFile, $end_directory)
+
+function splitPdfbyText($regex,$pdfFile, $end_directory)
 {
 	$fpdi 				= new \setasign\Fpdi\Fpdi();
 	$pdfParser 			= new \Smalot\PdfParser\Parser();
@@ -12,7 +13,7 @@ function splitPdfByEmployeeCode($pdfFile, $end_directory)
 	{
 		$pageParsed 	= $pagesPF[$currentPage-1]; 
 		$pageText 		= preg_replace('/(\s)/', '', $pageParsed->getText());
-		$employeeCode 	= getEmployeeCode($pageText);
+		$employeeCode 	= findRegex($regex, $pageText);
 
 		$employees[$employeeCode][] = $currentPage;
 
@@ -48,70 +49,9 @@ function splitPdfByEmployeeCode($pdfFile, $end_directory)
 	return $return;
 }
 
-function getEmployeeCode($pageText)
+function findRegex($regex, $pageText)
 {
-	$regex1 = '/((reg.)|(c(o|ó|Ó)d.)|(admiss(a|A|ã|Ã)o\d{4}\/))(\d{1,9})/i';
-
-	preg_match($regex1, $pageText, $pdfSearch);
-
-	if(count($pdfSearch) >= 3)
-		return end($pdfSearch);
-	else
-		return null;
-}
-
-function splitCartaoPontoByEmployeeCode($pdfFile, $end_directory)
-{
-	$fpdi 				= new \setasign\Fpdi\Fpdi();
-	$pdfParser 			= new \Smalot\PdfParser\Parser();
-	$parseFile 			= $pdfParser->parseFile($pdfFile);
-	$pagesPF  			= $parseFile->getPages();
-	$return 			= [];
-	$employees 			= [];
-
-	for ($currentPage = 1; $currentPage <= count($pagesPF); $currentPage++)
-	{
-		$pageParsed 	= $pagesPF[$currentPage-1]; 
-		$pageText 		= preg_replace('/(\s)/', '', $pageParsed->getText());
-		$employeeCode 	= getCartaoPontoEmployeeCode($pageText);
-
-		$employees[$employeeCode][] = $currentPage;
-
-		
-		if(!is_null($employeeCode))
-		{
-			$new_pdf 	= new \setasign\Fpdi\Fpdi();
-
-			$new_pdf->setSourceFile($pdfFile);
-
-			$pageId 	= $new_pdf->importPage($currentPage);
-			$size 		= $new_pdf->getTemplatesize($pageId);
-			$new_pdf->AddPage($size['orientation'], $size);
-			$new_pdf->useTemplate($pageId); 
-
-			try
-			{
-
-				$employeeFile	= $employeeCode.'_'.count($employees[$employeeCode]).".pdf";
-                //$employeeFile	= $employeeCode.".pdf";
-				$return[]		= $employeeFile;
-				$new_filename 	= $end_directory.$employeeFile;
-
-				$new_pdf->Output($new_filename, "F");
-			}
-			catch (Exception $e)
-			{
-				echo 'Caught exception: ',  $e->getMessage(), "\n";
-			}
-		}
-	}
-
-	return $return;
-}
-
-function getCartaoPontoEmployeeCode($pageText)
-{
-	$regex1 = '/((L|l)ocaliza(ç|Ç|c|C)(a|A|ã|Ã)o\:)(\d+)/i';
+	$regex1 = '/'.$regex.'/i';
 
 	preg_match($regex1, $pageText, $pdfSearch);
 
--- index.php
+++ index.php
@@ -5,8 +5,13 @@ ini_set('display_errors', 1);
 require __DIR__.'/vendor/autoload.php';
 require __DIR__.'/funcoes/splitPDF.php';
 
+//$arquivoTotal 	= isset($_POST['listagem']) ? $_POST['listagem'] : 'Contracheques.pdf' ;
 $arquivoTotal 	= isset($_POST['listagem']) ? $_POST['listagem'] : 'ponto.pdf' ;
-$end_directory 	= __DIR__."/conteudo/documentos/";
+$end_directory 	= __DIR__."/conteudo/documentos/ponto_";
 $pdfFile 		= __DIR__."/conteudo/documentos/$arquivoTotal";
 $ret 			= chmod($pdfFile, 0777);
-$arquivos 		= splitCartaoPontoByEmployeeCode($pdfFile, $end_directory);
+
+//$arquivos 		= splitPdfbyText('((reg.)|(c(o|ó|Ó)d.)|(admiss(a|A|ã|Ã)o\d{4}\/))(\d{1,9})',$pdfFile, $end_directory);
+$arquivos 		= splitPdfbyText('((L|l)ocaliza(ç|Ç|c|C)(a|A|ã|Ã)o\:)(\d+)',$pdfFile, $end_directory);
+
+echo "<pre>";print_r($arquivos);echo "</pre>";
